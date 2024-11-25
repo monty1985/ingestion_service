c37e145aa081e1f021f8a36f75123c69617ebf7f
Mohana Karthikeyan <mohanakumaar.k@gmail.com>
2024-11-25 15:30:09 -0800

    removing unnecesary item from the folder Structure

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env
-load_dotenv()
-
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "raw_device_data")
-
